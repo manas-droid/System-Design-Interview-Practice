d566ad748cfd8be4dfb17a75a4859acda9582f6d
Manas Kalangan <manas.kalangan@gmail.com>
2025-11-21 20:15:02 -0500

    Added functionality for triggering the webhook

--- webhook/src/webhook/webhook.controller.ts
+++ webhook/src/webhook/webhook.controller.ts
@@ -1,15 +1,15 @@
 import {Request, Response} from 'express';
 import { InMemoryWebhookRepository, IWebhookRepository, WebHookSubscriber } from './repository/webhook.inmemory.repository';
 
-interface WebHookRequest{
+interface WebHookRegistryRequest{
     webHookUrl:string,
     eventType:string
 }
 
-const webHookRegistry:IWebhookRepository = new InMemoryWebhookRepository()
+const webHookService:IWebhookRepository = new InMemoryWebhookRepository()
 
 export async function registryController(req:Request, res:Response){
-    const webHookRequest:WebHookRequest = req.body;
+    const webHookRequest:WebHookRegistryRequest = req.body;
     const webHookSubscriber:WebHookSubscriber = {
          webHookUrl : webHookRequest.webHookUrl,
          eventType : webHookRequest.eventType,
@@ -17,7 +17,7 @@ export async function registryController(req:Request, res:Response){
     }
 
    try{
-        await webHookRegistry.addToRepo(webHookSubscriber);
+        await webHookService.addToRepo(webHookSubscriber);
         res.status(200).json({ "message":"Webhook Registered" });
    } catch(error){
         res.status(500).json({"message": "Webhook registeration failed!"});
@@ -25,9 +25,24 @@ export async function registryController(req:Request, res:Response){
 
 }
 
-
+interface WebHookTriggerRequest {
+     eventType:string,
+     payload : any
+}
 
 
 export async function triggerController(req:Request, res:Response){
+     const triggerPayload: WebHookTriggerRequest = req.body;
+     const subcribers : WebHookSubscriber[] = await webHookService.getSubscribersByEventType(triggerPayload.eventType);
+     subcribers.forEach(async (sub: WebHookSubscriber)=>{
+          const response = await fetch(sub.webHookUrl, { method: "POST", body: triggerPayload.payload });
+          if(!response.ok){
+               throw new Error(`Something went wrong while triggering a callback : ${sub.webHookUrl} and status: ${response.status}`);
+          }
 
+          console.log(`Callback URL: ${sub.webHookUrl} triggered with response : ${response.status}`);
+     });
+
+
+     res.status(202).json({"message": "Webhook Triggered to " + subcribers.length + " subscribers" });
 }
